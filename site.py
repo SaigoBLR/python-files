084139f23b5baaad4707e386eea83d4288751054
SaigoBLR <metalgearacid@mail.ru>
2017-06-14 15:17:18 +0300

    ЗА...одностраничный сайт+шаблон

---
+++ site.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+#    return 'Hello, World!'
+
+#@app.route('/projects/')
+#def projects():
+#    return 'The project page'
+
+#@app.route('/about')
+#def about():
+#    return 'The about project page'
+
+@app.route('/templates/')
+@app.route('/templates/<name>')
+def hello(name=None):
+    #return render_template('template.html', name=name)
+    return render_template('template.html', name=name)
