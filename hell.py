4389edcb7db0d7382b25574f87008b75e83af1ed
SaigoBLR <metalgearacid@mail.ru>
2017-06-20 16:36:52 +0300

    вывод на печать Hello,<name>!

--- hell.py
+++ hell.py
@@ -1,19 +1,25 @@
+#from flask import Flask,render_template
+#app = Flask(__name__)
+
+#@app.route('/templates/')
+#@app.route('/templates/<name>')
+#def hello(name=None):
+#    return render_template('template.html', name=name)
+
+names = ["Fred", "Bob", "Alice"]
+
 from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/first.html')
+@app.route('/first.html/<name>')
+def first(name = None):
+    return render_template('first.html',name = names)
 
-@app.route('/projects/')
-def projects():
-    return 'The project page'
+@app.route('/second.html')
+#@app.route('/second/<name>')
+def second():
+    return render_template('second.html')
 
-@app.route('/about')
-def about():
-    return 'The about project page'
 
-@app.route('/templates/')
-@app.route('/templates/<name>')
-def hello(name=None):
-    return render_template('template.html', name=name)
+
