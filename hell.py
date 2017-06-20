f58f45edde0ca6ae72254c49ff190c4b5489ee07
SaigoBLR <metalgearacid@mail.ru>
2017-06-20 22:38:34 +0300

    Вывод новостей

--- hell.py
+++ hell.py
@@ -5,21 +5,25 @@
 #@app.route('/templates/<name>')
 #def hello(name=None):
 #    return render_template('template.html', name=name)
+class News:
+    def __init__(self,title,news):
+        self.title = title
+        self.news = news
 
-names = ["Fred", "Bob", "Alice"]
+p_news1 = News('Заголовок 1','Новость 1')
+p_news2 = News('Заголовок 2','Новость 2')
+p_news3 = News('Заголовок 3','Новость 3')
+
+lst = [p_news1,p_news2,p_news3]
 
 from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/first.html')
-@app.route('/first.html/<name>')
-def first(name = None):
-    return render_template('first.html',name = names)
+@app.route('/first.html/<news>')
+def first(news = None):
+    return render_template('first.html', news= lst)
 
-@app.route('/second.html')
-#@app.route('/second/<name>')
-def second():
-    return render_template('second.html')
 
 
 
