deb4baea529a624c405e72e9b001dd16cb517adf
SaigoBLR <metalgearacid@mail.ru>
2017-06-26 22:45:06 +0300

    deleted scripts

--- 123.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-def points():
-    f = open(input('Введите имя файла:'))
-    plt.figure("задание Андрея")                     #'ЗА.txt'
-    for line in f:
-        l,i = line.split(',')
-        plt.plot([l], [i], 'ro')
-        plt.axis([0, 20, 0, 50])
-    plt.show()
