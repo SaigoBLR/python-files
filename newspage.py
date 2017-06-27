12a8ebca833f07d0c4c39df1c9550668c1dd710d
SaigoBLR <metalgearacid@mail.ru>
2017-06-27 23:17:32 +0300

    corrected

--- newspage.py
+++ newspage.py
@@ -7,15 +7,15 @@ news1 = News('Заголовок 1','Новость 1')
 news2 = News('Заголовок 2','Новость 2')
 news3 = News('Заголовок 3','Новость 3')
 
-lst = [news1,news2,news3]
+news = [news1,news2,news3]
 
 from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/newstemple/')
-@app.route('/newstemple/<news>')
+#@app.route('/newstemple/<news>')
 def News(news = None):
-    return render_template('newstemple', news= lst)
+    return render_template('newstemple', news= news)
 
 
 
